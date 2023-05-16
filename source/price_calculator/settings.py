2d7358b3f969d335cb885d1242a0d06871d19ab7
Begaiym <begoshaaaa@gmail.com>
2023-05-16 22:00:00 +0600

    change settings

--- source/price_calculator/settings.py
+++ source/price_calculator/settings.py
@@ -154,6 +154,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER') # Your Gmail email address
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD') # Your Gmail password or app-specific password
-
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+LOGIN_URL = 'login'
